61f4711a166f9fad176ac405368ec2ddbc7ed0ab
SaadSalim <shuvrohossain8@gmail.com>
2020-10-31 21:19:48 +0600

    sign up changes

--- LibraryManagementSystem/signup.php
+++ LibraryManagementSystem/signup.php
@@ -1,104 +1,82 @@
-<?php include('server.php'); ?>
-
 <!DOCTYPE html>
-
-<html lang="en" dir="ltr">
-
+<html>
 <head>
-
+	<title>LMS</title>
+	<meta charset="utf-8" name="viewport" content="width=device-width,intial-scale=1">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-
-	<link href="https://fonts.googleapis.com/css2?family=Galada&family=Mukta:wght@300;700&display=swap"
-      rel="stylesheet"/>
-
-     <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-
-    <meta charset="UTF-8" />
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-	<title>Sign Up</title>
-
+	<style type="text/css">
+  		#side_bar{
+  			background-color: whitesmoke;
+  			padding: 50px;
+  			width: 300px;
+  			height: 450px;
+  		}
+  	</style>
 </head>
-
 <body>
-	<!-- <img src="b1.jpg"> -->
-
-
-<!-- Sign Up Starts-->
-	
-
-
-	
-
-	<form method="post" action="signup.php">
-		<!-- Display validation errors here -->
-		<div class="header">
-
-		<h2>Sign Up</h2>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="index.php">Library Management System(LMS)</a>
+			</div>
+			<ul class="nav navbar-nav navbar-right">
+				<li class="nav-item">
+					<a class="nav-link" href="index.php">Admin Login</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="index.php">User Login</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="index.php">Register</a>
+				</li>
+			</ul>
 		</div>
-		<?php include('errors.php'); ?>
-
-		<div class="input-group">
-			<label for="name">Name</label>
-			<input type="text" placeholder="Enter Your Name" id="name" name="name" value="<?php echo $name; ?>">
-		</div>
-
-		<div class="input-group">
-			<label for="username">User Name</label>
-			<input type="text" placeholder="Unique Username" id="username" name="username" value="<?php echo $username; ?>">
-		</div>
-
-		<div class="input-group">
-			<label for="email">Email</label>
-			<input type="text" placeholder="Enter Email" id="email" name="email" value="<?php echo $email; ?>">
-		</div>
-
-		<div>
-			<label for="gender">Gender</label>
-				<input type="radio" name="gender" value="male">Male
-				<input type="radio" name="gender" value="female">Female
-		</div>
-
-		<div class="input-group">
-			<label for="birthday">Date of Birth</label>
-			<input type="text" placeholder="(DD/MM/YYYY)" id="birthday" name="birthday">
-		</div>
-
-		<div class="input-group">
-			<label for="password">Password</label>
-			<input type="password" id="password" name="password">
-		</div>
-
-		<div class="input-group">
-			<label for="c_password">Confirm Password</label>
-			<input type="password" id="c_password" name="c_password">
-		</div>
-
-		<hr>
-
-   			<p class="para">By Creating An Account You Agree to Our <a href="Terms&Privacy.php">Terms & Privacy</a>.</p>
-   		
-
-		<div class="input-group">
-			<button type="submit" name="register" class="registerbtn">Sign Up</button>
-		</div>
-
- 		<div class="container signin">
-   			<p>Already have an account? <a href="login.php">Sign in</a>.</p>
- 		</div>
-	</form>
-	
-<!-- Sign Up Ends -->
-
-<!--
-
-<footer id="main-footer" class="main-footer">
-  <p>&copy; <i>Saad Ahmed Salim</i></p>
-</footer>
--->
+	</nav><br>
+	<span><marquee>This is library Management System. Library opens at 8:00 AM and close at 8:00 PM</marquee></span><br><br>
+	<div class="row">
+		<div class="col-md-4" id="side_bar">
+			<h5>Library Timing</h5>
+			<ul>
+				<li>Opening Timing: 8:00 AM</li>
+				<li>Closing Timing: 8:00 PM</li>
+				<li>(Sunday off)</li>
+			</ul>
+			<h5>What we provide ?</h5>
+			<ul>
+				<li>Full furniture</li>
+				<li>Free Wi-fi</li>
+				<li>News Papers</li>
+				<li>Discussion Room</li>
+				<li>RO Water</li>
+				<li>Peacefull Environment</li>
+			</ul>
+		</div>		
+		<div class="col-md-8" id="main_content">
+			<center><h3>User Registration Form</h3></center>
+			<form action="register.php" method="post">
+				<div class="form-group">
+					<label for="name">Full Name:</label>
+					<input type="text" name="name" class="form-control" required>
+				</div>
+				<div class="form-group">
+					<label for="name">Email ID:</label>
+					<input type="text" name="email" class="form-control" required>
+				</div>
+				<div class="form-group">
+					<label for="name">Password:</label>
+					<input type="password" name="password" class="form-control" required>
+				</div>
+				<div class="form-group">
+					<label for="name">Mobile Number:</label>
+					<input type="text" name="mobile" class="form-control" required>
+				</div>
+				<div class="form-group">
+					<label for="name">Address:</label>
+					<textarea rows="3" cols="40" class="form-control" name="address"></textarea>
+				</div>
+				<button type="submit" class="btn btn-primary">Register</button>
+			</form>
+	</div>
+	</div>
 </body>
-
 </html>
