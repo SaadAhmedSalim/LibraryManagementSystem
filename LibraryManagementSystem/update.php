1fcae3fc0a5179872a4a23776fbf5fdcdb1d3e16
SaadSalim <shuvrohossain8@gmail.com>
2020-11-26 23:12:14 +0600

    changes added

--- LibraryManagementSystem/update.php
+++ LibraryManagementSystem/update.php
@@ -2,11 +2,11 @@
 
 	$connection = mysqli_connect("localhost","root","");
 	$db = mysqli_select_db($connection,"library");
-	
-	$query = "update users set name='$_POST[name]',email='$_POST[email]',mobile='$_POST[mobile]',address='$_POST[address]' ";
+
+	$query = "UPDATE users SET name='$_POST[name]',email='$_POST[email]',mobile='$_POST[mobile]',address='$_POST[address]'";
 	$query_run = mysqli_query($connection,$query);
 
-	
+
 ?>
 <script type="text/javascript">
 	alert("Updated successfully...");
